aa81bbcffa88a324b481cf12963ad4bbb403ade0
hekgarcia <hekgarcia@gmail.com>
2017-04-25 22:49:22 -0500

    Adding changes for Youtube Search API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+
+const API_KEY = "AIzaSyBiS7_Z_x13vq-yKttwuzQDuI6uRndR8q0";
+
+YTSearch({key : API_KEY, term : 'macross'}, function(data) { console.log(data)})
 
 
 const App = () => {
