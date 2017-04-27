9be2b0340012eda00db7a80219c310fe35e7106f
hekgarcia <hekgarcia@gmail.com>
2017-04-27 10:27:30 -0500

    Adding changes for class component App

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js';
 
 const API_KEY = "AIzaSyBiS7_Z_x13vq-yKttwuzQDuI6uRndR8q0";
 
-YTSearch({key : API_KEY, term : 'macross'}, function(data) { console.log(data)})
 
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar/>
-        </div>
-    );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+        
+        YTSearch({key : API_KEY, term : 'macross'}, 
+                 (videos) => { 
+                    this.setState({ videos });
+                    // this.setState( {videos: videos} );
+                    //console.log(data)
+                });    
+    }
     
+    render() {
+        return  <div>
+                    <SearchBar/>
+                    <VideoList videos={this.state.videos} />
+                </div>
+    }
 }
  
 ReactDOM.render(
